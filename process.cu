d1095347363b529acda0e1cc87bafe2386a62cb9
we0091234 <447587096@qq.com>
2023-01-07 21:34:43 +0800

    cuda postprocessing

---
+++ process.cu
@@ -0,0 +1,223 @@
+#include <opencv2/opencv.hpp>
+
+    const int NUM_BOX_ELEMENT = 7;      // left, top, right, bottom, confidence, class, keepflag
+    static __device__ void affine_project(float* matrix, float x, float y, float* ox, float* oy){
+        *ox = matrix[0] * x + matrix[1] * y + matrix[2];
+        *oy = matrix[3] * x + matrix[4] * y + matrix[5];
+    }
+     static __global__ void decode_kernel(float* predict, int num_bboxes, int num_classes, float confidence_threshold, float* invert_affine_matrix, float* parray, int max_objects){  
+
+        int position = blockDim.x * blockIdx.x + threadIdx.x;
+		if (position >= num_bboxes) return;
+
+        float* pitem     = predict + (4 + num_classes) * position;
+        // float objectness = pitem[4];
+        // if(objectness < confidence_threshold)
+        //     return;
+
+        float* class_confidence = pitem + 4;
+        float confidence        = *class_confidence++;
+        int label               = 0;
+        for(int i = 1; i < num_classes; ++i, ++class_confidence){
+            if(*class_confidence > confidence){
+                confidence = *class_confidence;
+                label      = i;
+            }
+        }
+
+        // confidence *= objectness;
+        if(confidence < confidence_threshold)
+            return;
+   
+        int index = atomicAdd(parray, 1);
+        if(index >= max_objects)
+            return;
+        // printf("index %d max_objects %d\n", index,max_objects);
+        float cx         = pitem[0];
+        float cy         = pitem[1];
+        float width      = pitem[2];
+        float height     = pitem[3];
+
+        float left   = cx - width * 0.5f;
+        float top    = cy - height * 0.5f;
+        float right  = cx + width * 0.5f;
+        float bottom = cy + height * 0.5f;
+
+        affine_project(invert_affine_matrix, left,  top,    &left,  &top);
+        affine_project(invert_affine_matrix, right, bottom, &right, &bottom);
+
+
+        float* pout_item = parray + 1 + index * NUM_BOX_ELEMENT;
+        *pout_item++ = left;
+        *pout_item++ = top;
+        *pout_item++ = right;
+        *pout_item++ = bottom;
+        *pout_item++ = confidence;
+        *pout_item++ = label;
+        *pout_item++ = 1; // 1 = keep, 0 = ignore
+    }
+
+    static __device__ float box_iou(
+        float aleft, float atop, float aright, float abottom, 
+        float bleft, float btop, float bright, float bbottom
+    ){
+
+        float cleft 	= max(aleft, bleft);
+        float ctop 		= max(atop, btop);
+        float cright 	= min(aright, bright);
+        float cbottom 	= min(abottom, bbottom);
+        
+        float c_area = max(cright - cleft, 0.0f) * max(cbottom - ctop, 0.0f);
+        if(c_area == 0.0f)
+            return 0.0f;
+        
+        float a_area = max(0.0f, aright - aleft) * max(0.0f, abottom - atop);
+        float b_area = max(0.0f, bright - bleft) * max(0.0f, bbottom - btop);
+        return c_area / (a_area + b_area - c_area);
+    }
+
+    static __global__ void nms_kernel(float* bboxes, int max_objects, float threshold){
+
+        int position = (blockDim.x * blockIdx.x + threadIdx.x);
+        int count = min((int)*bboxes, max_objects);
+        if (position >= count) 
+            return;
+        
+        // left, top, right, bottom, confidence, class, keepflag
+        float* pcurrent = bboxes + 1 + position * NUM_BOX_ELEMENT;
+        for(int i = 0; i < count; ++i){
+            float* pitem = bboxes + 1 + i * NUM_BOX_ELEMENT;
+            if(i == position || pcurrent[5] != pitem[5]) continue;
+
+            if(pitem[4] >= pcurrent[4]){
+                if(pitem[4] == pcurrent[4] && i < position)
+                    continue;
+
+                float iou = box_iou(
+                    pcurrent[0], pcurrent[1], pcurrent[2], pcurrent[3],
+                    pitem[0],    pitem[1],    pitem[2],    pitem[3]
+                );
+
+                if(iou > threshold){
+                    pcurrent[6] = 0;  // 1=keep, 0=ignore
+                    return;
+                }
+            }
+        }
+    } 
+
+
+__global__ void warpaffine_kernel( 
+    uint8_t* src, int src_line_size, int src_width, 
+    int src_height, float* dst, int dst_width, 
+    int dst_height, uint8_t const_value_st,
+    float * d2i, int edge) {
+    int position = blockDim.x * blockIdx.x + threadIdx.x;
+    if (position >= edge) return;
+
+    float m_x1 = d2i[0];
+    float m_y1 = d2i[1];
+    float m_z1 = d2i[2];
+    float m_x2 = d2i[3];
+    float m_y2 = d2i[4];
+    float m_z2 = d2i[5];
+
+    int dx = position % dst_width;
+    int dy = position / dst_width;
+    float src_x = m_x1 * dx + m_y1 * dy + m_z1 + 0.5f;
+    float src_y = m_x2 * dx + m_y2 * dy + m_z2 + 0.5f;
+    float c0, c1, c2;
+
+    if (src_x <= -1 || src_x >= src_width || src_y <= -1 || src_y >= src_height) {
+        // out of range
+        c0 = const_value_st;
+        c1 = const_value_st;
+        c2 = const_value_st;
+    } else {
+        int y_low = floorf(src_y);
+        int x_low = floorf(src_x);
+        int y_high = y_low + 1;
+        int x_high = x_low + 1;
+
+        uint8_t const_value[] = {const_value_st, const_value_st, const_value_st};
+        float ly = src_y - y_low;
+        float lx = src_x - x_low;
+        float hy = 1 - ly;
+        float hx = 1 - lx;
+        float w1 = hy * hx, w2 = hy * lx, w3 = ly * hx, w4 = ly * lx;
+        uint8_t* v1 = const_value;
+        uint8_t* v2 = const_value;
+        uint8_t* v3 = const_value;
+        uint8_t* v4 = const_value;
+
+        if (y_low >= 0) {
+            if (x_low >= 0)
+                v1 = src + y_low * src_line_size + x_low * 3;
+
+            if (x_high < src_width)
+                v2 = src + y_low * src_line_size + x_high * 3;
+        }
+
+        if (y_high < src_height) {
+            if (x_low >= 0)
+                v3 = src + y_high * src_line_size + x_low * 3;
+
+            if (x_high < src_width)
+                v4 = src + y_high * src_line_size + x_high * 3;
+        }
+
+        c0 = w1 * v1[0] + w2 * v2[0] + w3 * v3[0] + w4 * v4[0];
+        c1 = w1 * v1[1] + w2 * v2[1] + w3 * v3[1] + w4 * v4[1];
+        c2 = w1 * v1[2] + w2 * v2[2] + w3 * v3[2] + w4 * v4[2];
+    }
+
+    //bgr to rgb 
+    float t = c2;
+    c2 = c0;
+    c0 = t;
+
+    //normalization
+    c0 = c0 / 255.0f;
+    c1 = c1 / 255.0f;
+    c2 = c2 / 255.0f;
+
+    //rgbrgbrgb to rrrgggbbb
+    int area = dst_width * dst_height;
+    float* pdst_c0 = dst + dy * dst_width + dx;
+    float* pdst_c1 = pdst_c0 + area;
+    float* pdst_c2 = pdst_c1 + area;
+    *pdst_c0 = c0;
+    *pdst_c1 = c1;
+    *pdst_c2 = c2;
+}
+
+void preprocess_kernel_img(
+    uint8_t* src, int src_width, int src_height,
+    float* dst, int dst_width, int dst_height,
+    float *d2i,cudaStream_t stream) {
+    
+    int jobs = dst_height * dst_width;
+    int threads = 256;
+    int blocks = ceil(jobs / (float)threads);
+    warpaffine_kernel<<<blocks, threads, 0, stream>>>(
+        src, src_width*3, src_width,
+        src_height, dst, dst_width,
+        dst_height, 128, d2i, jobs);
+
+}
+
+   void decode_kernel_invoker(float* predict, int num_bboxes, int num_classes, float confidence_threshold, float* invert_affine_matrix, float* parray, int max_objects, cudaStream_t stream)
+    {
+        int block = 256;
+        int  grid =  ceil(num_bboxes / (float)block);
+        
+        decode_kernel<<<grid, block, 0, stream>>>(predict, num_bboxes, num_classes, confidence_threshold, invert_affine_matrix, parray, max_objects);
+    }
+
+      void nms_kernel_invoker(float* parray, float nms_threshold, int max_objects, cudaStream_t stream){
+        
+        
+        int block = max_objects<256? max_objects:256;
+        int grid = ceil(max_objects / (float)block);
+        nms_kernel<<<grid, block, 0, stream>>>(parray, max_objects, nms_threshold);
+    }
--- yolov8.cpp
+++ yolov8.cpp
@@ -10,7 +10,10 @@
 #include "cuda_runtime_api.h"
 #include "logging.h"
 #include "include/utils.hpp"
-#include "preprocess.h"
+#include <cuda_runtime.h>
+#include <cstdint>
+#define MAX_OBJECTS 1024
+#define NUM_BOX_ELEMENT 7
 #define MAX_IMAGE_INPUT_SIZE_THRESH 5000 * 5000
 
 
@@ -40,214 +43,54 @@ const char* INPUT_BLOB_NAME = "images"; //onnx 输入  名字
 const char* OUTPUT_BLOB_NAME = "output0"; //onnx 输出 名字
 static Logger gLogger;
 
-struct Object
+struct affine_matrix  //前处理仿射变换矩阵和逆矩阵
 {
-    cv::Rect_<float> rect; 
-    int label;
-    float prob;
+    float i2d[6];   //仿射变换正矩阵
+    float d2i[6];   //仿射变换逆矩阵
+
 };
 
-
-static inline float intersection_area(const Object& a, const Object& b)
+struct bbox 
 {
-    cv::Rect_<float> inter = a.rect & b.rect;
-    return inter.area();
-}
-
-static void qsort_descent_inplace(std::vector<Object>& faceobjects, int left, int right)
-{
-    int i = left;
-    int j = right;
-    float p = faceobjects[(left + right) / 2].prob;
-
-    while (i <= j)
-    {
-        while (faceobjects[i].prob > p)
-            i++;
-
-        while (faceobjects[j].prob < p)
-            j--;
-
-        if (i <= j)
-        {
-            // swap
-            std::swap(faceobjects[i], faceobjects[j]);
-
-            i++;
-            j--;
-        }
-    }
-
-    #pragma omp parallel sections
-    {
-        #pragma omp section
-        {
-            if (left < j) qsort_descent_inplace(faceobjects, left, j);
-        }
-        #pragma omp section
-        {
-            if (i < right) qsort_descent_inplace(faceobjects, i, right);
-        }
-    }
-}
-
-static void qsort_descent_inplace(std::vector<Object>& objects)
-{
-    if (objects.empty())
-        return;
-
-    qsort_descent_inplace(objects, 0, objects.size() - 1);
-}
-
-static void nms_sorted_bboxes(const std::vector<Object>& faceobjects, std::vector<int>& picked, float nms_threshold)
-{
-    picked.clear();
-
-    const int n = faceobjects.size();
-
-    std::vector<float> areas(n);
-    for (int i = 0; i < n; i++)
-    {
-        areas[i] = faceobjects[i].rect.area();
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        const Object& a = faceobjects[i];
-
-        int keep = 1;
-        for (int j = 0; j < (int)picked.size(); j++)
-        {
-            const Object& b = faceobjects[picked[j]];
-
-            // intersection over union
-            float inter_area = intersection_area(a, b);
-            float union_area = areas[i] + areas[picked[j]] - inter_area;
-            // float IoU = inter_area / union_area
-            if (inter_area / union_area > nms_threshold)
-                keep = 0;
-        }
-
-        if (keep)
-            picked.push_back(i);
-    }
-}
-
-
-
-int find_max(float *prob,int num) //找到类别
-{
-    int max= 0;
-    for(int i=1; i<num; i++)
-    {
-        if (prob[max]<prob[i])
-         max = i;
-    }
-
-    return max;
-
-}
-
-
-static void generate_yolo_proposals(float *feat_blob, float prob_threshold,
-                                     std::vector<Object> &objects,int OUTPUT_CANDIDATES) {
-  const int num_class = NUM_CLASSES;  
-  const int ckpt=0  ; 
-
-  const int num_anchors = OUTPUT_CANDIDATES;
-
-  for (int anchor_idx = 0; anchor_idx < num_anchors; anchor_idx++) {
-    // const int basic_pos = anchor_idx * (num_class + 5 + 1);
-    // float box_objectness = feat_blob[basic_pos + 4];
-
-    // int cls_id = feat_blob[basic_pos + 5];
-    // float score = feat_blob[basic_pos + 5 + 1 + cls_id];
-    // score *= box_objectness;
-
-
-    const int basic_pos = anchor_idx * (num_class + 4 + ckpt); //5代表 x,y,w,h,object_score  ckpt代表5个关键点 每个关键点3个数据
-    // float box_objectness = feat_blob[basic_pos + 4];
-
-    // int cls_id = find_max(&feat_blob[basic_pos +5+ckpt],num_class);   //找到类别v5
-    int cls_id = find_max(&feat_blob[basic_pos +4],num_class);   //v7
-    // float score = feat_blob[basic_pos + 5 +8 + cls_id]; //v5
-    float score = feat_blob[basic_pos + 4 + cls_id];  //v7
-    // score *= box_objectness; 
-
-
-    if (score > prob_threshold) {
-      // yolox/models/yolo_head.py decode logic
-      float x_center = feat_blob[basic_pos + 0];
-      float y_center = feat_blob[basic_pos + 1];
-      float w = feat_blob[basic_pos + 2];
-      float h = feat_blob[basic_pos + 3];
-      float x0 = x_center - w * 0.5f;
-      float y0 = y_center - h * 0.5f;
-      
-    //   float *landmarks=&feat_blob[basic_pos +5]; //v5
-    float *landmarks=&feat_blob[basic_pos +5+num_class];
-
-      Object obj;
-      obj.rect.x = x0;
-      obj.rect.y = y0;
-      obj.rect.width = w;
-      obj.rect.height = h;
-      obj.label = cls_id;
-      obj.prob = score;
-      objects.push_back(obj);
-    }
-  }
-}
-
-static void decode_outputs(float* prob, std::vector<Object>& objects, float scale, const int img_w, const int img_h,int OUTPUT_CANDIDATES,int top,int left) {
-        std::vector<Object> proposals;
-        generate_yolo_proposals(prob,  BBOX_CONF_THRESH, proposals,OUTPUT_CANDIDATES);
-        qsort_descent_inplace(proposals);
-        std::vector<int> picked;
-        nms_sorted_bboxes(proposals, picked, NMS_THRESH);
-        int count = picked.size();
-        objects.resize(count);
-        for (int i = 0; i < count; i++)
-        {
-            objects[i] = proposals[picked[i]];
-
-            // adjust offset to original unpadded
-            float x0 = (objects[i].rect.x-left) / scale;
-            float y0 = (objects[i].rect.y-top) / scale;
-            float x1 = (objects[i].rect.x + objects[i].rect.width-left) / scale;
-            float y1 = (objects[i].rect.y + objects[i].rect.height-top) / scale;
-    
-            // clip
-            x0 = std::max(std::min(x0, (float)(img_w - 1)), 0.f);
-            y0 = std::max(std::min(y0, (float)(img_h - 1)), 0.f);
-            x1 = std::max(std::min(x1, (float)(img_w - 1)), 0.f);
-            y1 = std::max(std::min(y1, (float)(img_h - 1)), 0.f);
-
-            objects[i].rect.x = x0;
-            objects[i].rect.y = y0;
-            objects[i].rect.width = x1 - x0;
-            objects[i].rect.height = y1 - y0;
-        }
-}
-
-const float color_list[5][3] =
-{
-    {255, 0, 0},
-    {0, 255, 0},
-    {0, 0, 255},
-    {0, 255, 255},
-    {255,255,0},
+     float x1,x2,y1,y2;
+     float score;
+     int label;
 };
 
+void preprocess_kernel_img(uint8_t* src, int src_width, int src_height,
+                           float* dst, int dst_width, int dst_height,
+                           float *d2i,cudaStream_t stream);       //letter_box 前处理 用cuda
 
-void doInference_cu(IExecutionContext& context, cudaStream_t& stream, void **buffers, float* output, int batchSize,int OUTPUT_SIZE) {
-    // infer on the batch asynchronously, and DMA output back to host
-    context.enqueue(batchSize, buffers, stream, nullptr);
-    CHECK(cudaMemcpyAsync(output, buffers[1], batchSize * OUTPUT_SIZE * sizeof(float), cudaMemcpyDeviceToHost, stream));
-    cudaStreamSynchronize(stream);
+ void decode_kernel_invoker(
+        float* predict, int num_bboxes, int num_classes,float confidence_threshold, 
+        float* invert_affine_matrix, float* parray,
+        int max_objects, cudaStream_t stream
+    );                                           //后处理 cuda
+
+void nms_kernel_invoker(
+        float* parray, float nms_threshold, int max_objects, cudaStream_t stream
+    );                            //nums cuda
+
+void affine_project(float *d2i,float x,float y ,float *ox,float *oy)  //通过仿射变换逆矩阵返回原图上的坐标
+{
+  *ox = x*d2i[0]+y*d2i[1]+d2i[2];
+  *oy = x*d2i[3]+y*d2i[4]+d2i[5];
 }
 
-
+void get_affine_martrix(affine_matrix &afmt,cv::Size &to,cv::Size &from)  //计算放射变换的正矩阵和逆矩阵
+{
+    float scale= std::min(to.width/(float)from.width,to.height/(float)from.height);
+    afmt.i2d[0]=scale;
+    afmt.i2d[1]=0;
+    afmt.i2d[2]=(-scale*from.width+to.width)*0.5;
+    afmt.i2d[3]=0;
+    afmt.i2d[4]=scale;
+    afmt.i2d[5]=(-scale*from.height+to.height)*0.5;
+    cv::Mat  cv_i2d(2,3,CV_32F,afmt.i2d);
+    cv::Mat  cv_d2i(2,3,CV_32F,afmt.d2i);
+    cv::invertAffineTransform(cv_i2d,cv_d2i);         //通过opencv获取仿射变换逆矩阵
+    memcpy(afmt.d2i,cv_d2i.ptr<float>(0),sizeof(afmt.d2i));
+}
 
 int main(int argc, char** argv)
  {
@@ -286,6 +129,21 @@ int main(int argc, char** argv)
     assert(inputIndex == 0);
     assert(outputIndex == 1);
     // Create GPU buffers on device
+
+    float *affine_matrix_d2i_host= nullptr;  
+    float *affine_matrix_d2i_device=nullptr;
+
+    float *decode_ptr_host=nullptr;
+    float *decode_ptr_device=nullptr;
+
+    CHECK(cudaMallocHost(&affine_matrix_d2i_host,sizeof(float)*6));  //仿射变换逆矩阵数组长度是6
+    CHECK(cudaMalloc(&affine_matrix_d2i_device,sizeof(float)*6)) ; 
+
+    decode_ptr_host = new float[1+MAX_OBJECTS*NUM_BOX_ELEMENT];
+
+    CHECK(cudaMalloc(&decode_ptr_device,sizeof(float)*(1+MAX_OBJECTS*NUM_BOX_ELEMENT)));
+
+    
    
 
     auto out_dims = engine_det->getBindingDimensions(1);
@@ -313,7 +171,7 @@ int main(int argc, char** argv)
 
    
 
-    static float* prob = new float[output_size];
+
 
 
     // std::string imgPath ="/mnt/Gpan/Mydata/pytorchPorject/Chinese_license_plate_detection_recognition/imgs";
@@ -324,42 +182,68 @@ int main(int argc, char** argv)
     readFileList(const_cast<char *>(imgPath.c_str()),imagList,fileType);
     double sumTime = 0;
     int index = 0;
+    cv::Size to(INPUT_W,INPUT_H);
     for (auto &input_image_path:imagList) 
     {
-        
+        affine_matrix afmt;
         cv::Mat img = cv::imread(input_image_path);
-          double begin_time = cv::getTickCount();
-         float *buffer_idx = (float*)buffers[inputIndex];
+        double begin_time = cv::getTickCount();
+        cv::Size from(img.cols,img.rows);
+        get_affine_martrix(afmt,to,from);
+
+        memcpy(affine_matrix_d2i_host,afmt.d2i,sizeof(afmt.d2i));
+        CHECK(cudaMemcpyAsync(affine_matrix_d2i_device,affine_matrix_d2i_host,sizeof(afmt.d2i),cudaMemcpyHostToDevice,stream));
+        
+        float *buffer_idx = (float*)buffers[inputIndex];
         size_t size_image = img.cols * img.rows * 3;
         size_t size_image_dst = INPUT_H * INPUT_W * 3;
         memcpy(img_host, img.data, size_image);
-       
+
         CHECK(cudaMemcpyAsync(img_device, img_host, size_image, cudaMemcpyHostToDevice, stream));
-        preprocess_kernel_img(img_device, img.cols, img.rows, buffer_idx, INPUT_W, INPUT_H, stream);
+        preprocess_kernel_img(img_device, img.cols, img.rows, buffer_idx, INPUT_W, INPUT_H,affine_matrix_d2i_device, stream);
         double time_pre = cv::getTickCount();
         double time_pre_=(time_pre-begin_time)/cv::getTickFrequency()*1000;
-        // std::cout<<"preprocessing time is "<<time_pre_<<" ms"<<std::endl;
+        (*context_det).enqueueV2((void**)buffers, stream, nullptr);
+        float *predict = buffers[outputIndex];
+        CHECK(cudaMemsetAsync(decode_ptr_device,0,sizeof(int),stream));
+        decode_kernel_invoker(predict,OUTPUT_CANDIDATES,NUM_CLASSES,BBOX_CONF_THRESH,affine_matrix_d2i_device,decode_ptr_device,MAX_OBJECTS,stream);
+        nms_kernel_invoker(decode_ptr_device, NMS_THRESH, MAX_OBJECTS, stream);//cuda nms
+        CHECK(cudaMemcpyAsync(decode_ptr_host,decode_ptr_device,sizeof(float)*(1+MAX_OBJECTS*NUM_BOX_ELEMENT),cudaMemcpyDeviceToHost,stream));
+        cudaStreamSynchronize(stream);
       
-        doInference_cu(*context_det,stream, (void**)buffers,prob,1,output_size);
-
-        float r = std::min(INPUT_W / (img.cols*1.0), INPUT_H / (img.rows*1.0));
-        int unpad_w = r * img.cols;
-        int unpad_h = r * img.rows;
-        int left = (INPUT_W-unpad_w)/2;
-        int top = (INPUT_H-unpad_h)/2; 
-        int img_w = img.cols;
-        int img_h = img.rows;
-        float scale = std::min(INPUT_W / (img.cols*1.0), INPUT_H / (img.rows*1.0));
-        
-        std::vector<Object> objects;
-        decode_outputs(prob, objects, scale, img_w, img_h,OUTPUT_CANDIDATES,top,left);
         std::cout<<input_image_path<<" ";
+
+        std::vector<bbox> boxes;
+               
+        int boxes_count=0;
+        int count = std::min((int)*decode_ptr_host,MAX_OBJECTS);
         
-        for (int i = 0; i<objects.size(); i++)
+
+         for (int i = 0; i<count;i++)
         {
-            cv::rectangle(img, objects[i].rect, cv::Scalar(0,255,0), 2);
-            cv::putText(img, std::to_string((int)objects[i].label), cv::Point(objects[i].rect.x, objects[i].rect.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+           int basic_pos = 1+i*NUM_BOX_ELEMENT;
+           int keep_flag= decode_ptr_host[basic_pos+6];
+           if (keep_flag==1)
+           {
+             boxes_count+=1;
+             bbox  box;
+             box.x1 =  decode_ptr_host[basic_pos+0];
+             box.y1 =  decode_ptr_host[basic_pos+1];
+             box.x2 =  decode_ptr_host[basic_pos+2];
+             box.y2 =  decode_ptr_host[basic_pos+3];
+             box.score=decode_ptr_host[basic_pos+4];
+             box.label=decode_ptr_host[basic_pos+5];
+             boxes.push_back(box);
+           }
         }
+
+         for (int i = 0; i<boxes_count; i++)
+        {
+            cv::Rect roi_area(boxes[i].x1,boxes[i].y1,boxes[i].x2-boxes[i].x1,boxes[i].y2-boxes[i].y1);
+            cv::rectangle(img, roi_area, cv::Scalar(0,255,0), 2);
+              cv::putText(img, std::to_string((int)boxes[i].label), cv::Point(boxes[i].x1, boxes[i].y1 - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+        }
+
           double end_time = cv::getTickCount();
           auto time_gap = (end_time-begin_time)/cv::getTickFrequency()*1000;
         std::cout<<"  time_gap: "<<time_gap<<"ms ";
@@ -388,6 +272,10 @@ int main(int argc, char** argv)
     CHECK(cudaFreeHost(img_host));
     CHECK(cudaFree(buffers[inputIndex]));
     CHECK(cudaFree(buffers[outputIndex]));
-    delete [] prob;
+    CHECK(cudaFree(decode_ptr_device));
+    CHECK(cudaFree(affine_matrix_d2i_device));
+    CHECK(cudaFreeHost(affine_matrix_d2i_host));
+
+    delete [] decode_ptr_host;
     return 0;
 }
